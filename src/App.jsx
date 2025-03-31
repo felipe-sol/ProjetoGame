14cd20dd4f24156364640e0d5c7d75c467e1e8e3
Felipe <labsfiap@fiap.com.br>
2025-03-31 11:45:56 -0300

    Finalizando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-
-
-
+import Login from './routes/Login'
+import Error from './routes/Error'
+import Jogador from './routes/Jogador'
+import Home from './routes/Home'
+import Aside from './routes/Aside'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Router>
+        <Header/>
+        <Routes>
+            <Route path="*" element={<Error/>}/>
+            <Route path="/" element={<Home/>}/> 
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/filtro" element={<Filtro/>}/>
+            <Route path="/jogador" element={<Jogador/>}/>
+        </Routes>
+        <Aside/>
+        <footer/>
+      </Router>
+      <Aside/>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <h1>
+        
+      </h1>
+
     </div>
   )
 }
